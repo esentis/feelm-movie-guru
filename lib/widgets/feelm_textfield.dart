498f572dd52263dfb33da604e0be4bf7e85ae1a0
George Leonidis <esentakos@yahoo.gr>
2021-03-16 00:14:40 +0200

    Creates custom TextField widget

---
+++ lib/widgets/feelm_textfield.dart
@@ -0,0 +1,49 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:feelm/constants.dart';
+import 'package:feelm/theme_config.dart';
+import 'package:flutter/material.dart';
+
+class FeelmTextField extends StatelessWidget {
+  final BuildContext context;
+  final String label;
+  const FeelmTextField({
+    @required this.context,
+    @required this.label,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      textAlign: TextAlign.center,
+      style: kStyleLight.copyWith(
+        color: ThemeProvider.of(context).brightness == Brightness.light
+            ? purpleDark
+            : pink,
+      ),
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(60),
+          borderSide: BorderSide(
+            color: purpleDark,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(
+            60,
+          ),
+          borderSide: BorderSide(
+            color: Theme.of(context).scaffoldBackgroundColor,
+            width: 3,
+          ),
+        ),
+        labelText: label,
+        labelStyle: kStyleLight.copyWith(
+          color: ThemeProvider.of(context).brightness == Brightness.light
+              ? purpleDark
+              : whiteLight,
+        ),
+      ),
+    );
+  }
+}
