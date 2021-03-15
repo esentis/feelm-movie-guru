3c6aedda899052952f7adc344e93d46b70bb9c17
George Leonidis <esentakos@yahoo.gr>
2021-03-16 00:15:03 +0200

    Creates constant textstyle

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
 var kLog = Logger();
+
+TextStyle kStyleLight = const TextStyle(
+  fontSize: 20,
+  fontFamily: 'JosefinSans',
+  fontWeight: FontWeight.w100,
+);
