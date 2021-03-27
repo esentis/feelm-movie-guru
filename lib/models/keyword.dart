edfa4bf1aa237c6d0e494f8f9c0157c789c9754c
George Leonidis <esentakos@yahoo.gr>
2021-03-27 19:40:23 +0200

    🗃 Creates Keyword model

---
+++ lib/models/keyword.dart
@@ -0,0 +1,34 @@
+class KeywordResults {
+  KeywordResults({
+    required this.keywords,
+  });
+
+  List<Keyword> keywords;
+
+  factory KeywordResults.fromJson(Map<String, dynamic> json) => KeywordResults(
+        keywords:
+            List<Keyword>.from(json['results'].map((x) => Keyword.fromJson(x))),
+      );
+
+  factory KeywordResults.error() => KeywordResults(keywords: []);
+}
+
+class Keyword {
+  Keyword({
+    required this.name,
+    required this.id,
+  });
+
+  String name;
+  int id;
+
+  factory Keyword.fromJson(Map<String, dynamic> json) => Keyword(
+        name: json['name'],
+        id: json['id'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'id': id,
+      };
+}
