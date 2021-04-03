2433c3af5e1b64753bfa7d32997021a8656b18f3
George Leonidis <esentakos@yahoo.gr>
2021-04-03 23:17:22 +0300

    ✨Adds Zodiac sign model

---
+++ lib/models/sign.dart
@@ -0,0 +1,29 @@
+import 'package:feelm/models/keyword.dart';
+
+class ZodiacSign {
+  String name;
+  String image;
+  List<Keyword> keywords;
+  DateTime from;
+  DateTime to;
+  ZodiacSign({
+    required this.from,
+    required this.keywords,
+    required this.name,
+    required this.to,
+    required this.image,
+  });
+
+  factory ZodiacSign.fromMap(Map<String, dynamic>? map) => ZodiacSign(
+        from: map!['from'].toDate(),
+        keywords: List.generate(
+          map['keywords'].length,
+          (index) => Keyword.fromJson(
+            map['keywords'][index],
+          ),
+        ),
+        name: map['name'],
+        to: map['to'].toDate(),
+        image: map['image'] ?? '',
+      );
+}
