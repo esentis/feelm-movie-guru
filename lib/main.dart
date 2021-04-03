5783f87abcf88e0e9ce5f2316e4444728c3428c5
George Leonidis <esentakos@yahoo.gr>
2021-04-03 23:17:58 +0300

    ✨Adds keyword and sign provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:ui';
+import 'package:feelm/constants.dart';
+import 'package:feelm/models/keyword.dart';
+import 'package:feelm/models/sign.dart';
 import 'package:feelm/pages/landing_page.dart';
-import 'package:feelm/providers/keywords_provider.dart';
+import 'package:feelm/providers/signs_and_keywords.dart';
 import 'package:feelm/theme_config.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // ignore: library_prefixes
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
@@ -10,6 +14,8 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await DotEnv.load(fileName: '.env');
   // check if is running on Web
   if (kIsWeb) {
@@ -32,8 +38,21 @@ class MyApp extends StatelessWidget {
     var initTheme = isPlatformDark ? darkTheme : lightTheme;
     return MultiProvider(
       providers: [
-        Provider<KeywordsProvider>(
-          create: (_) => KeywordsProvider(),
+        FutureProvider<List<Keyword>>(
+          initialData: [],
+          catchError: (context, x) {
+            kLog.e(x);
+            return [];
+          },
+          create: (_) => getKeywords(),
+        ),
+        FutureProvider<List<ZodiacSign>>(
+          initialData: [],
+          catchError: (context, x) {
+            kLog.e(x);
+            return [];
+          },
+          create: (_) => getSigns(),
         )
       ],
       child: MaterialApp(
