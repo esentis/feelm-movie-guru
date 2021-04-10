86879c58808c7c2efafdf0c7078941d511d8f735
George Leonidis <esentakos@yahoo.gr>
2021-04-11 01:15:14 +0300

    ✨Creates method to get zodiac sign

--- lib/constants.dart
+++ lib/constants.dart
@@ -27,3 +27,62 @@ Future<AccessToken?> signInWithFacebook() async {
   kLog.e(result.message);
   return null;
 }
+
+String getSign(DateTime birthDate) {
+  // Checking for Aries
+  if ((birthDate.month == 3 && birthDate.day >= 21) ||
+      (birthDate.month == 4 && birthDate.day <= 19)) {
+    return 'Aries';
+  }
+  // Checking for Taurus
+  if ((birthDate.month == 4 && birthDate.day >= 20) ||
+      (birthDate.month == 5 && birthDate.day <= 20)) {
+    return 'Taurus';
+  }
+  // Checking for Gemini
+  if ((birthDate.month == 5 && birthDate.day >= 21) ||
+      (birthDate.month == 6 && birthDate.day <= 20)) {
+    return 'Gemini';
+  }
+  // Checking for Cancer
+  if ((birthDate.month == 6 && birthDate.day >= 21) ||
+      (birthDate.month == 7 && birthDate.day <= 22)) {
+    return 'Cancer';
+  }
+  // Checking for Leo
+  if ((birthDate.month == 7 && birthDate.day >= 23) ||
+      (birthDate.month == 8 && birthDate.day <= 22)) {
+    return 'Leo';
+  }
+  // Checking for Virgo
+  if ((birthDate.month == 8 && birthDate.day >= 23) ||
+      (birthDate.month == 9 && birthDate.day <= 22)) {
+    return 'Virgo';
+  }
+  // Checking for Libra
+  if ((birthDate.month == 9 && birthDate.day >= 23) ||
+      (birthDate.month == 10 && birthDate.day <= 21)) {
+    return 'Libra';
+  }
+  // Checking for Scorpio
+  if ((birthDate.month == 10 && birthDate.day >= 23) ||
+      (birthDate.month == 11 && birthDate.day <= 21)) {
+    return 'Scorpio';
+  }
+  // Checking for Sagittarius
+  if ((birthDate.month == 11 && birthDate.day >= 22) ||
+      (birthDate.month == 12 && birthDate.day <= 21)) {
+    return 'Sagittarius';
+  }
+  // Checking for Capricorn
+  if ((birthDate.month == 12 && birthDate.day >= 22) ||
+      (birthDate.month == 1 && birthDate.day <= 18)) {
+    return 'Capricorn';
+  }
+  // Checking for Pisces
+  if ((birthDate.month == 2 && birthDate.day >= 19) ||
+      (birthDate.month == 3 && birthDate.day <= 20)) {
+    return 'Pisces';
+  }
+  return 'ok';
+}
