6dc2c96ddeb3878bbc83d2ab02fbfc2ee0d5d14d
George Leonidis <esentakos@yahoo.gr>
2021-04-29 22:27:25 +0300

    ✨Creates favorites model and methods

---
+++ lib/models/favorites.dart
@@ -0,0 +1,74 @@
+import 'package:feelm/constants.dart';
+
+var favorites = kFirestore.collection('favorites');
+
+class Favorite {
+  String email;
+  int movieId;
+  Favorite({
+    required this.email,
+    required this.movieId,
+  });
+
+  factory Favorite.fromMap(Map<String, dynamic> map) => Favorite(
+        email: map['email'],
+        movieId: map['movieId'],
+      );
+}
+
+Future<List<Favorite>> getUserFavorites() async {
+  // ignore: omit_local_variable_types
+  List<Favorite> favList = [];
+  await favorites
+      .where('email', isEqualTo: kAuth.currentUser!.email)
+      .get()
+      .then(
+    (qs) {
+      if (qs.docs.isNotEmpty) {
+        // ignore: omit_local_variable_types
+
+        qs.docs.forEach(
+          (snap) {
+            favList.add(Favorite.fromMap(snap.data()!));
+          },
+        );
+      }
+    },
+  );
+  return favList;
+}
+
+Future<void> toggleFavorite(Favorite favorite) async {
+  var docRefForDeletion = '';
+  await favorites.where('email', isEqualTo: favorite.email).get().then(
+    (qs) {
+      if (qs.docs.isNotEmpty) {
+        // ignore: omit_local_variable_types
+        List<Favorite> favorites = [];
+        qs.docs.forEach(
+          (snap) {
+            var fav = Favorite.fromMap(snap.data()!);
+            if (fav.movieId == favorite.movieId &&
+                favorite.email == fav.email) {
+              docRefForDeletion = snap.reference.id;
+            } else {
+              favorites.add(fav);
+            }
+          },
+        );
+      }
+    },
+  );
+  if (docRefForDeletion.isEmpty) {
+    kLog.w('Adding favorite for ${favorite.email} movie ${favorite.movieId}');
+    await favorites.add(
+      {
+        'email': favorite.email,
+        'movieId': favorite.movieId,
+      },
+    );
+  } else {
+    kLog.w('Removing favorite with doc reference $docRefForDeletion');
+    await favorites.doc(docRefForDeletion).delete();
+  }
+}
