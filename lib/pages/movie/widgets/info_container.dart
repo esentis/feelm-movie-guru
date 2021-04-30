22ae7e09b34b5a44c7a68c1f375c5131862deacf
George Leonidis <esentakos@yahoo.gr>
2021-04-30 17:37:19 +0300

    ✨Creates info widget container

---
+++ lib/pages/movie/widgets/info_container.dart
@@ -0,0 +1,60 @@
+import 'package:feelm/constants.dart';
+import 'package:flutter/material.dart';
+
+class InfoContainer extends StatelessWidget {
+  final IconData icon;
+  final double iconSize;
+  final String text;
+
+  final String subtitleText;
+  final double subtitleTextSize;
+
+  final double space;
+  final Color iconColor;
+
+  const InfoContainer({
+    required this.icon,
+    required this.text,
+    required this.iconColor,
+    required this.subtitleText,
+    this.iconSize = 25,
+    this.space = 3,
+    this.subtitleTextSize = 17,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: iconColor,
+          size: iconSize,
+        ),
+        SizedBox(
+          width: space,
+        ),
+        Column(
+          children: [
+            Text(
+              text,
+              style: kStyleLight.copyWith(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: kColorMain,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              subtitleText,
+              style: kStyleLight.copyWith(
+                fontSize: subtitleTextSize,
+                color: kColorMain,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
