97a08428e562d265c2d0766c3f553c5cc50e3b6a
George Leonidis <esentakos@yahoo.gr>
2021-06-17 21:45:43 +0300

    ➖ Removes Google fonts dependency

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:logger/logger.dart';
 import 'package:supercharged/supercharged.dart';
@@ -14,7 +13,8 @@ FirebaseFirestore kFirestore = FirebaseFirestore.instance;
 var kLog = Logger();
 var kAuth = FirebaseAuth.instance;
 
-TextStyle kStyleLight = GoogleFonts.openSansCondensed(
+TextStyle kStyleLight = const TextStyle(
+  fontFamily: 'JosefinSans',
   fontSize: 20,
   fontWeight: FontWeight.w100,
 );
